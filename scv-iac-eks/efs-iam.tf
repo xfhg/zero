fcd8c63cbc438d2359654d202b6c29891191e272
Chris Scheible <cgscheible@yahoo.com>
2021-07-18 18:36:59 +0800

    scv-iac-eks/efs-iam.tf - initial

--- scv-iac-eks/efs-iam.tf
+++ scv-iac-eks/efs-iam.tf
@@ -101,8 +101,3 @@ resource "aws_efs_file_system_policy" "eks-efs-policy" {
 POLICY
 }
 
-resource "aws_efs_mount_target" "efs_mount_targets" {
-  file_system_id  = module.efs.id
-  subnet_id       = var.subnet_ids[count.index]
-  security_groups = [ module.efs.security_group_id ]
-}
