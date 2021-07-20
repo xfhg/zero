31beaf38f1b90c4d31349e58df4e65e61a7dd63d
Chris Scheible <cgscheible@yahoo.com>
2021-07-20 16:29:35 +0800

    scv-iac-eks/terraform.tf - rename TF-state key back to lab

--- scv-iac-eks/terraform.tf
+++ scv-iac-eks/terraform.tf
@@ -4,8 +4,7 @@ terraform {
   backend "s3" {
     region         = "ap-southeast-1"
     bucket         = "scventures-development-baseline-olea-state"
-    #key            = "lab.tfstate"
-    key            = "eks.tfstate"
+    key            = "lab.tfstate"
     dynamodb_table = "scventures-development-baseline-olea-state-lock"
     profile        = ""
     role_arn       = ""
